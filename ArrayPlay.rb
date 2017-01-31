f1a87baf5926c78fc244bb965ab711fe4ba692d5
klocode <kendrick.t.lo@gmail.com>
2017-01-31 13:03:06 -0500

    Starting code, created student array

---
+++ ArrayPlay.rb
@@ -0,0 +1,16 @@
+our_class = [
+  {name: "Rob"},
+  {name: "David"},
+  {name: "Nancy"},
+  {name: "Kalea"},
+  {name: "Laura"},
+  {name: "Dave"},
+  {name: "Demetra"},
+  {name: "Kendrick"},
+  {name: "Phil"},
+  {name: "Ben"},
+  {name: "Michael"},
+  {name: "Miguel"}
+]
+
+our_class
