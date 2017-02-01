7dbcd9da5157ce21985759e661d47fc308955cb0
klocode <kendrick.t.lo@gmail.com>
2017-01-31 20:36:07 -0500

    testing out starring method and working on epic challenges

--- ArrayPlay.rb
+++ ArrayPlay.rb
@@ -1,11 +1,15 @@
 
 #methods
 def four_words(text)
-  text.split.select {|four| four.length == 4}
+  how_many_words(text, 4)
 end
 
 def how_many_words(text, num)
   text.split.select {|word| word.length == num}
+end
+
+def starring?(mov, actor)
+  mov.select {|movie| movie[:stars].include? actor}.collect {|title| title[:title]}
 end
 
 #Beginning of the assignment
@@ -55,11 +59,20 @@ puts movies_less_than_100.inspect
 leonardo = movies.select {|movie| movie[:stars].include? "Leonardo DiCaprio"}.collect {|mov| mov[:title]}
 puts leonardo.inspect
 
-puts four_words(sentence).inspect
+puts "Words of length 4 characters are #{four_words(sentence).inspect}"
 
-puts how_many_words(sentence, 2).inspect
+puts "The words of asked length are #{how_many_words(sentence, 2).inspect}"
 
 money = 0
 movies.each {|mon| money += mon[:budget]}
 
 puts "The total budget for the movies is #{money}!"
+
+actors = []
+
+#two steps: take an array to work off of, parse said array for actors and put those actors into a hash in my actor array
+#assign the results of starring? to a :movie hash in actors array
+
+puts starring?(movies, "Leonardo DiCaprio").inspect
+puts starring?(movies, "Kate Winslet").inspect
+puts starring?(movies, "Tom Hanks").inspect
